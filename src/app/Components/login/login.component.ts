3cb45967017d15e5d1510a1d1ddb7ad90138419b
harsh kalyani <harshkalyani1998@gmail.com>
2025-05-22 13:15:32 +0530

    Login component: Initial UI commit

---
+++ src/app/Components/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  public loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    })
+  }
+
+  public onLogin(): void {
+    const { username, password } = this.loginForm.value;
+
+    if (username === 'harsh@gmail.com' && password === 'harsh') {
+      this.router.navigate(['/expenses']);
+    } else {
+      alert('Invalid credentials');
+    }
+  }
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    {
-    path: '',
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadComponent: () => import('./Components/login/login.component').then(m => m.LoginComponent) },
+  {
+    path: 'expenses',
     loadComponent: () => import('./Components/expense-list/expense-list.component')
       .then(m => m.ExpenseListComponent)
   }
