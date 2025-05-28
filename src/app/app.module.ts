09da8e878cfa535567ea48cb46b6367046f36348
harsh kalyani <harshkalyani1998@gmail.com>
2025-05-28 10:30:51 +0530

    Loader Interceptor: Integration of loadet with every api call with help of interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ConfigServiceService } from './Services/config-service/config-service.service';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { LoaderInterceptor } from './Interceptors/loader.interceptor';
 
 export function loadConfig(config: ConfigServiceService) {
   return () => config.loadConfig();
@@ -24,10 +25,15 @@ export function loadConfig(config: ConfigServiceService) {
     MatSnackBarModule,
   ],
   providers: [
-        {
+    {
       provide: APP_INITIALIZER,
       useFactory: loadConfig,
       deps: [ConfigServiceService],
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoaderInterceptor,
       multi: true
     }
   ],
